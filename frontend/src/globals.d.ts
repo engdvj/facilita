054104c9fbd7b4da3154f798d8e4e48b5aded1d3
Davi Jr <153877813+engdvj@users.noreply.github.com>
2025-06-05 19:24:56 -0300

    Create dedicated admin pages

--- frontend/src/globals.d.ts
+++ frontend/src/globals.d.ts
@@ -29,6 +29,7 @@ declare module 'react-router-dom' {
   export const BrowserRouter: any
   export const Routes: any
   export const Route: any
+  export const Outlet: any
   export const Link: any
   export const useNavigate: any
 }
