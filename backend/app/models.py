c3fbfd0596c3158af09aea931b71186de48fb088
Davi Jr <153877813+engdvj@users.noreply.github.com>
2025-06-08 02:27:42 -0300

    Refine admin pages and color format

--- backend/app/models.py
+++ backend/app/models.py
@@ -45,3 +45,4 @@ class Link(db.Model):
 class Color(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.String(30), unique=True, nullable=False)
+    name = db.Column(db.String(50))
